1b1b5a676fd29773cc2063e23c7a1dc0990b2fbf
Anastasiia Pashkova <anastasia.pashkova2003@gmail.com>
2025-03-26 14:11:35 +0100

    fix: add one more parameter for write_to_file

--- app/io/output.py
+++ app/io/output.py
@@ -7,12 +7,13 @@ def print_to_console(text):
     """
     print(text)
 
-def write_to_file(text):
+def write_to_file(text, file_path='data/output.txt'):
     """
     Function to write text to a file using built-in Python capabilities.
     
     Args:
     text (str): Text to be written to the file
+    file_path (str): Path to the file where text will be written (default: 'data/output.txt')
     """
-    with open('data/output.txt', 'w') as file:
+    with open(file_path, 'w') as file:
         file.write(text)
